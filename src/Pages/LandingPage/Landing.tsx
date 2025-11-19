e160c6f4d8bcf64b074f30d277f3fa1d4115c085
HARSHJYOT_KAUR <68178422+MAHI21-WEB@users.noreply.github.com>
2025-11-19 14:46:26 +0530

    Devfolio Button

--- src/Pages/LandingPage/Landing.tsx
+++ src/Pages/LandingPage/Landing.tsx
@@ -37,12 +37,12 @@ function Landing() {
 					7-8th Feb 2026
 				</h1>
 				<div className="mt-10 flex flex-col gap-5">
-					<div 
+					<button
 						class="apply-button" 
 						data-hackathon-slug="hacktu-7" 
 						data-button-theme="light"
 						style="height: 44px; width: 100%; max-width: 312px"
-					></div>
+					></button>
 					<button
 						className="font-space-grotesk hover:scale-105 transition-all font-light px-14 rounded-md py-3 bg-[#A0BED3]"
 						onClick={() => window.open("https://hacktu-6.devfolio.co/")}
